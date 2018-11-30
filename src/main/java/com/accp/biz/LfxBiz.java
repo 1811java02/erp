ccd2b198f45c38013ec769778f8d5d2ecf8063ae
yantaoshi <yantaoshi@qq.com>
2018-11-30 17:28:11 +0800

    2018 11 30 刘芳雄

--- src/main/java/com/accp/biz/LfxBiz.java
+++ src/main/java/com/accp/biz/LfxBiz.java
@@ -1,4 +1,19 @@
 package com.accp.biz;
 
+import com.accp.dao.LfxDao;
+import com.accp.pojo.T_UNIT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LfxBiz {
+    @Autowired
+    private LfxDao dao;
+
+    public T_UNIT queryunit(Integer page){
+        return dao.queryunit(page);
+    }
+
 }
