a3f0e403227635592e452c25cacb641d08c68bb0
pp87716 <877164449@qq.com>
2018-11-30 18:06:37 +0800

    11月30日郑石平第二次提交

--- src/main/java/com/accp/action/ZspAction.java
+++ src/main/java/com/accp/action/ZspAction.java
@@ -1,4 +1,21 @@
 package com.accp.action;
 
+import com.accp.biz.ZspBiz;
+import com.accp.dao.ZspDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/zsp/query")
 public class ZspAction {
+    @Autowired
+     private ZspBiz biz;
+    @RequestMapping("/get")
+    @ResponseBody
+    public String get(){
+        System.out.print("*************************************************");
+       return  biz.get();
+    }
 }
