5fade7a3adc3ce7f675f2f12a652fb23bb2211d8
yuno <1799729439@qq.com>
2018-12-03 13:51:02 +0800

    v1.0.0

--- src/main/java/com/accp/action/LhyAction.java
+++ src/main/java/com/accp/action/LhyAction.java
@@ -1,26 +1,47 @@
 package com.accp.action;
 
 import com.accp.biz.LhyBiz;
-import com.accp.vo.lhy.T_DSPRODUCT;
+import com.accp.pojo.T_CUSTOMER_INFORMATION;
 import com.accp.pojo.T_DSPRODUCTCLASS;
+import com.accp.pojo.T_WAREHOUSE;
+import com.accp.vo.lhy.T_DSPRODUCT;
+import com.accp.vo.lhy.T_PERSONNEL;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("")
+import java.util.List;
+
+@RestController
+@RequestMapping("/lhy")
 public class LhyAction {
 	@Autowired
 	private LhyBiz biz;
 
-	@RequestMapping("/lhy/query/dsproduct")
+	@RequestMapping("/query/dsproduct")
 	public PageInfo<T_DSPRODUCT> queryDsproduct(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "1") Integer size) {
 		return biz.queryDsproduct(page, size);
 	}
 
-	@RequestMapping("/lhy/query/dsproductclass")
+	@RequestMapping("/query/dsproductclass")
 	public PageInfo<T_DSPRODUCTCLASS> queryDsproductclass(@RequestParam(defaultValue = "1") Integer page) {
 		return biz.queryDsproductclass(page);
+	}
+
+	@RequestMapping("/query/customer")
+	public List<T_CUSTOMER_INFORMATION> queryCustomer() {
+		return biz.queryCustomer();
+	}
+
+	@RequestMapping("/query/warehouse")
+	public List<T_WAREHOUSE> queryWarehouse() {
+		return biz.queryWarehouse();
+	}
+
+	@RequestMapping("/query/personnel")
+	public List<T_PERSONNEL> queryPersonnel() {
+		return biz.queryPersonnel();
 	}
 }
---
+++ src/main/java/com/accp/vo/lhy/T_PERSONNEL.java
@@ -0,0 +1,15 @@
+package com.accp.vo.lhy;
+
+import com.accp.pojo.T_DEPARTMENT;
+
+public class T_PERSONNEL extends com.accp.pojo.T_PERSONNEL {
+	private T_DEPARTMENT department;
+
+	public T_DEPARTMENT getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(T_DEPARTMENT department) {
+		this.department = department;
+	}
+}
