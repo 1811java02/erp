a54b54b0ec8641a1ff47cacf98ba836a4a89bd92
pp87716 <877164449@qq.com>
2018-12-03 17:42:56 +0800

    12月3日郑石平第一次提交

--- src/main/java/com/accp/vo/zsp/T_PURCHASE_PURCHASEVo.java
+++ src/main/java/com/accp/vo/zsp/T_PURCHASE_PURCHASEVo.java
@@ -1,19 +1,36 @@
 package com.accp.vo.zsp;
 
-import com.accp.pojo.T_PURCHASE_PURCHASE;
 import com.accp.pojo.T_PURCHASE_PURCHASE_DETAILS;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class T_PURCHASE_PURCHASEVo extends com.accp.pojo.T_PURCHASE_PURCHASE {
-      private List<T_PURCHASE_PURCHASE_DETAILS> list=new ArrayList<T_PURCHASE_PURCHASE_DETAILS>();
+      private List<DsProductVo> list=new ArrayList<DsProductVo>();
+      private String personname ;
+      private String departname;
 
-    public void setList(List<T_PURCHASE_PURCHASE_DETAILS> list) {
+    public List<DsProductVo> getList() {
+        return list;
+    }
+
+    public String getPersonname() {
+        return personname;
+    }
+
+    public String getDepartname() {
+        return departname;
+    }
+
+    public void setList(List<DsProductVo> list) {
         this.list = list;
     }
 
-    public List<T_PURCHASE_PURCHASE_DETAILS> getList() {
-        return list;
+    public void setPersonname(String personname) {
+        this.personname = personname;
+    }
+
+    public void setDepartname(String departname) {
+        this.departname = departname;
     }
 }
