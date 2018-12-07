bbb9a6e732bed729b5714e87a91453f964754d6b
yuno <1799729439@qq,com>
2018-12-07 17:09:40 +0800

    v1.0.0

--- src/main/java/com/accp/vo/lhy/T_DSDSSALEMAIN.java
+++ src/main/java/com/accp/vo/lhy/T_DSDSSALEMAIN.java
@@ -1,11 +1,17 @@
 package com.accp.vo.lhy;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.Date;
 import java.util.List;
 
 /**
  * 销售出库主单
  */
 public class T_DSDSSALEMAIN extends com.accp.pojo.T_DSDSSALEMAIN {
+	@JSONField(format = "yyyy-MM-dd")
+	private Date billdate;
+
 	private List<T_DSDSSALESUB> dsdssalesub;
 
 	public List<T_DSDSSALESUB> getDsdssalesub() {
@@ -14,5 +20,15 @@ public class T_DSDSSALEMAIN extends com.accp.pojo.T_DSDSSALEMAIN {
 
 	public void setDsdssalesub(List<T_DSDSSALESUB> dsdssalesub) {
 		this.dsdssalesub = dsdssalesub;
+	}
+
+	@Override
+	public Date getBilldate() {
+		return billdate;
+	}
+
+	@Override
+	public void setBilldate(Date billdate) {
+		this.billdate = billdate;
 	}
 }
